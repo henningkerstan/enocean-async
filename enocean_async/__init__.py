55aaef266eb1718c4557cd44294d3bcd162ff705
Henning Kerstan <mail@henningkerstan.de>
2026-04-11 19:59:55 +0200

    Bump version: 0.13.0 → 0.13.1-dev0

--- enocean_async/__init__.py
+++ enocean_async/__init__.py
@@ -2,7 +2,7 @@
 An async implementation of the EnOcean Serial Protocol Version 3.
 """
 
-__version__ = "0.13.0"
+__version__ = "0.13.1-dev0"
 __date__ = "2026-04-11"
 
 from .address import EURID, BaseAddress, BroadcastAddress, SenderAddress
